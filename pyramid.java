6960d6b09bf800d55f74175f34f3b8bf2052f66e
ashutosh6120 <ashutosh4064@gmail.com>
2021-03-06 11:42:46 +0530

    added pyramid program

---
+++ pyramid.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class pyramid    //class declaration
+{
+    public static void printstars(int n)
+    {
+        int i, j;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("* ");      // using * to print
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n = 8;     // size of the input
+        printstars(n);
+    }
+}
